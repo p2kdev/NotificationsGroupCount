7e0338f3e9bd97943ca94d5b82f57ce658560bb2
p2kdev <p2kdev@gmail.com>
2025-02-09 20:55:58 -0800

    font adjustment

--- NGCBadgeView.m
+++ NGCBadgeView.m
@@ -74,9 +74,9 @@ CGFloat ngcBadgeSizeHigherThan10 = 30;
     // Hide if text is empty or zero
     self.hidden = (text == nil || [text isEqualToString:@""] || [text isEqualToString:@"0"]);
     if ([text integerValue] > 99) {
-        self.badgeLabel.font = [UIFont boldSystemFontOfSize:8.0];
+        self.badgeLabel.font = [UIFont boldSystemFontOfSize:9.0];
     } else if ([text integerValue] > 9) {   
-        self.badgeLabel.font = [UIFont boldSystemFontOfSize:10.0];
+        self.badgeLabel.font = [UIFont boldSystemFontOfSize:11];
     } else {
         self.badgeLabel.font = [UIFont boldSystemFontOfSize:13.0];
     }
